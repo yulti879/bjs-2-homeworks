07913c0079d34be6fba82882206a38f3eed38673
yulti879 <yulti879@gmail.com>
2024-04-16 17:21:22 +0300

    Первое задание успешно выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,25 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = b ** 2 - 4 * a * c;
-	let x1 = (-b + Math.sqrt(d)) / (2 * a);
-	let x1 = (-b - Math.sqrt(d)) / (2 * a);
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;  
 
-	if (d < 0) {
-		arr = [];
-	} else if (d === 0) {
-		arr = [x1];
-	} else {
-		arr = [x1, x2];
-	};
+  if (d === 0) {
+    arr.push (-b / 2 * a);
+  } else if (d > 0) {
+    arr.push ((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+  };
 
-	return arr;
+  return arr;
 };
 
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let hundredth = percent / 12 / 100;
-	let creditBody = amount - contribution;
-	let monthlyPayment = creditBody * (hundredth + (hundredth / (((1 + hundredth) ** countMonths) - 1)));
-	let totalMortgage = monthlyPayment * countMonths;
+  let hundredth = percent / 12 / 100;
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (hundredth + (hundredth / (((1 + hundredth) ** countMonths) - 1)));
+  let totalMortgage = monthlyPayment * countMonths;
 
-	return (Number(totalMortgage.toFixed(2)));
-}
+  return (Number(totalMortgage.toFixed(2)));
+};
