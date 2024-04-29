9ce40732b59d82875a4b7c9d635e001d13f3b8da
yulti879 <yulti879@gmail.com>
2024-04-30 00:15:38 +0300

    Внесены правки в третье задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -77,7 +77,7 @@ function averageEvenElementsWorker(...arr) {
 
 function makeWork(arrOfArr, func) {
 	let maxWorkerResult = -Infinity;
-	for (i = 1; i < arrOfArr.length; i++) {
+	for (let i = 0; i < arrOfArr.length; i++) {
 		const result = func(...arrOfArr[i]);
 		if (result > maxWorkerResult) {
 			maxWorkerResult = result;
